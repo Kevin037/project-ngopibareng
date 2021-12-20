d56a209577ca79bfc53146676d85185ae28907d0
Kevin Satria Muhammad Iqbal <48180192+Kevin037@users.noreply.github.com>
2021-12-21 06:17:04 +0700

    Update foot.blade.php

--- resources/views/foot.blade.php
+++ resources/views/foot.blade.php
@@ -53,21 +53,5 @@
           });
       }, false);
   })();
-
-  $(function () {
-    $("#example1").DataTable({
-      "responsive": true, "lengthChange": false, "autoWidth": false,
-      "buttons": ["print"]
-    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
-      "responsive": true,
-    });
-  });
 </script>
 
